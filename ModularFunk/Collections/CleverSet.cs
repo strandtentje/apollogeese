0a826387427e0008bcfe731f4afdbca9b41a383f
Rob Tierolff <teh@borreh.nl>
2014-05-24 00:54:53 +0200

    Implemented a cleverset. I'm not sure what it does, it's probably clever, and it'll probably get tossed out at some point.

---
+++ ModularFunk/Collections/CleverSet.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using BorrehSoft.Utensils.Collections.Maps.Search;
+
+namespace BorrehSoft.Utensils.Collections
+{
+	/// <summary>
+	/// Hash set.
+	/// </summary>
+	public class CleverSet<T> : Dictionary<string, T> where T : IIndexable
+	{
+		public CleverSet ()
+		{
+		}
+
+		new public void Add (T item)
+		{
+			this.Add(item.Meta, item);
+		}
+
+		/// <summary>
+		/// Produces a sorted list from this hashed list.
+		/// </summary>
+		/// <returns>
+		/// The sorted list.
+		/// </returns>
+		public SortedList<string, T> ToSortedList ()
+		{
+			return new SortedList<string, T>(this);
+		}		
+
+		/// <summary>
+		/// Preserves only items also in the specified set.
+		/// </summary>
+		/// <param name='nextSet'>
+		/// The list with the items which need to be preserved
+		/// </param>
+		public void PreserveOnlyFrom (CleverSet<T> nextSet)
+		{
+			IEnumerable<string> keyCache = base.Keys;
+				
+			foreach(string key in keyCache)
+				if (!nextSet.ContainsKey(key))
+					base.Remove(key);
+		}
+
+		/// <summary>
+		/// Adds from the specified set.
+		/// </summary>
+		/// <param name='nextSet'>
+		/// The list from which items are going to be added to this set.
+		/// </param>
+		public void AddFrom (CleverSet<T> nextSet)
+		{
+			foreach(string key in nextSet.Keys)						
+				if (!base.ContainsKey(key))
+					base.Add(key, nextSet[key]);
+		}
+	}
+}
+
