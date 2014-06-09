d9c249c1b98d5e833fb7e49460df9be30266145f
Rob Tierolff <teh@borreh.nl>
2014-06-09 03:42:31 +0200

    Implemented querying machine that branches once per row.

--- BasicWeblings/Site/Page/DataDisplay/Querier.cs
+++ BasicWeblings/Site/Page/DataDisplay/Querier.cs
@@ -5,12 +5,22 @@ using BorrehSoft.Utensils.Collections.Settings;
 using BorrehSoft.Extensions.BasicWeblings.Server;
 using L = BorrehSoft.Utensils.Log.Secretary;
 using BorrehSoft.ApolloGeese.Duckling.Http;
+using System.Data;
+using System.Data.Common;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO;
 
 namespace BorrehSoft.Extensions.BasicWeblings.Site.Page.DataDisplay
 {
+	/// <summary>
+	/// Querier; queries.
+	/// </summary>
 	public class Querier : Service
 	{
 		private Service iterator = Stub;
+		private Service successful = Stub;
 
 		public override string Description {
 			get {
@@ -18,34 +28,100 @@ namespace BorrehSoft.Extensions.BasicWeblings.Site.Page.DataDisplay
 			}
 		}
 
+		/// <summary>
+		/// The connection string template.
+		/// </summary>
+		public const string ConnectionStringTemplate = 
+			"Server={0}; Database={1}; User ID={2}; Password={3}; Pooling={4};";
+
+		/// <summary>
+		/// Gets the connection string.
+		/// </summary>
+		/// <value>
+		/// The connection string.
+		/// </value>
+		public string ConnectionString { get; private set; }
+
+		/// <summary>
+		/// Gets the connection.
+		/// </summary>
+		/// <value>
+		/// The connection.
+		/// </value>
+		public IDbConnection Connection { get; private set; }
+
+		/// <summary>
+		/// Gets the query text.
+		/// </summary>
+		/// <value>
+		/// The query text.
+		/// </value>
+		public string QueryText { get; private set; }
+
+		public List<string> OrderedParameters { get; private set; }
+
 		protected override void Initialize (Settings modSettings)
 		{
-			Branches["Iterator"] = Stub;
+			Branches ["iterator"] = Stub;
+			Branches ["successful"] = Stub;
+
+			ConnectionString = string.Format (ConnectionStringTemplate, 
+			                                 modSettings ["host"],
+			                                 modSettings ["db"],
+			                                 modSettings ["user"],
+			                                 modSettings ["pass"],
+			                                 bool.Parse (modSettings.GetString("pool", "False")));
+
+			Connection = new MySqlConnection (ConnectionString);
+
+			Connection.Open ();
+
+			QueryText = File.ReadAllText((string)modSettings ["query"]);
+
+			OrderedParameters = new List<string> ();
+
+			if (modSettings.Has ("params")) {
+				List<object> parameters = (List<object>)modSettings ["params"];
+				foreach(object p in parameters) OrderedParameters.Add((string)p);
+			}
 		}
 
 		protected override void HandleBranchChanged (object sender, ItemChangedEventArgs<Service> e)
 		{
-			if (e.Name == "Iterator") iterator = e.NewValue;
+			if (e.Name == "iterator") iterator = e.NewValue;
+			if (e.Name == "successful") successful = e.NewValue;
 		}
 
 
 		protected override bool Process (IInteraction parameters)
 		{
-			IHttpInteraction castInteraction = (IHttpInteraction)parameters;
+			IDbCommand Command = Connection.CreateCommand ();
+			Command.CommandText = QueryText;
+			Command.Prepare ();
 
-			if (castInteraction == null) {
-				L.Report(4, "Incoming data was of incorrect type");
-				return false;
+			foreach (string paramname in OrderedParameters) {
+				IDbDataParameter parameter = Command.CreateParameter ();
+				parameter.ParameterName = paramname;
+				parameter.Value = parameters [paramname];
+				Command.Parameters.Add (parameter);
 			}
 
+			IDataReader reader = Command.ExecuteReader();
+			ResultInteraction resultline = null;
 
-			if (generator.TryProcess (generatorInteraction)) {
+			bool success = true;
 
-
-			} else {
-				L.Report(4, "Unable to generate data from parameters");
-				return false;
+			while (reader.Read()) 
+			{
+				resultline = new ResultInteraction (parameters, reader);
+				success &= iterator.TryProcess(resultline);
 			}
+
+			reader.Close();
+
+			if (success && (successful != Stub)) successful.TryProcess(parameters);
+
+			return success;
 		}
 	}
 }
