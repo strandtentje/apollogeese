3925c81c86546fb0cad36f9c603dfde6802cb853
Rob Tierolff <teh@borreh.nl>
2014-08-30 13:41:45 +0200

    Querier now initializes in a slightly more sane way. Query result branching distinguishes between no results, one result, multiple results and overall success now.

--- BasicWeblings/Site/Page/DataDisplay/Querier.cs
+++ BasicWeblings/Site/Page/DataDisplay/Querier.cs
@@ -19,8 +19,7 @@ namespace BorrehSoft.Extensions.BasicWeblings.Site.Page.DataDisplay
 	/// </summary>
 	public class Querier : Service
 	{
-		private Service iterator = Stub;
-		private Service successful = Stub;
+		private Service none, single, iterator, successful;
 
 		public override string Description {
 			get {
@@ -62,66 +61,148 @@ namespace BorrehSoft.Extensions.BasicWeblings.Site.Page.DataDisplay
 
 		protected override void Initialize (Settings modSettings)
 		{
+			Branches ["none"] = Stub;
+			Branches ["single"] = Stub;
 			Branches ["iterator"] = Stub;
 			Branches ["successful"] = Stub;
 
-			ConnectionString = string.Format (ConnectionStringTemplate, 
-			                                 modSettings ["host"],
-			                                 modSettings ["db"],
-			                                 modSettings ["user"],
-			                                 modSettings ["pass"],
-			                                 (bool)modSettings.GetBool("pool", true));
+			InitializeConnection(
+					modSettings ["host"], modSettings ["db"], 			                     
+					modSettings ["user"], modSettings ["pass"],
+					(bool)modSettings.GetBool("pool", true));
 
-			Connection = new MySqlConnection (ConnectionString);
-
-			Connection.Open ();
-
-			QueryText = File.ReadAllText((string)modSettings ["query"]);
-
-			OrderedParameters = new List<string> ();
-
-			if (modSettings.Has ("params")) {
-				List<object> parameters = (List<object>)modSettings ["params"];
-				foreach(object p in parameters) OrderedParameters.Add((string)p);
-			}
+			InitializeQuery(modSettings["query"],
+			             modSettings.Get("params", null) as List<object>);
 		}
 
+		/// <summary>
+		/// Initializes the connection.
+		/// </summary>
+		/// <param name='databasehost'>
+		/// Databasehost.
+		/// </param>
+		/// <param name='databasename'>
+		/// Databasename.
+		/// </param>
+		/// <param name='user'>
+		/// User.
+		/// </param>
+		/// <param name='password'>
+		/// Password.
+		/// </param>
+		/// <param name='usepool'>
+		/// Usepool.
+		/// </param>
+		private void InitializeConnection (string databasehost, string databasename, string user, string password, bool usepool)
+		{
+			ConnectionString = string.Format (ConnectionStringTemplate, databasehost, databasename, user, password, usepool);
+
+			Connection = new MySqlConnection (ConnectionString);
+			Connection.Open ();
+		}
+
+		/// <summary>
+		/// Prepares the query.
+		/// </summary>
+		/// <param name='QueryTextFile'>
+		/// Query text file.
+		/// </param>
+		/// <param name='queryParameters'>
+		/// Query parameters.
+		/// </param>
+		private void InitializeQuery (string QueryTextFile, List<object> queryParameters = null)
+		{
+			this.QueryText = File.ReadAllText(QueryTextFile);
+
+			this.OrderedParameters = new List<string> ();
+
+			if (queryParameters != null) 
+				foreach(object p in queryParameters) 
+					this.OrderedParameters.Add((string)p);
+		}
+
+		/// <summary>
+		/// Handles the branch changed.
+		/// </summary>
+		/// <param name='sender'>
+		/// Sender.
+		/// </param>
+		/// <param name='e'>
+		/// E.
+		/// </param>
 		protected override void HandleBranchChanged (object sender, ItemChangedEventArgs<Service> e)
 		{
+			if (e.Name == "none") none = e.NewValue;
+			if (e.Name == "single") single = e.NewValue;
 			if (e.Name == "iterator") iterator = e.NewValue;
 			if (e.Name == "successful") successful = e.NewValue;
 		}
 
-
-		protected override bool Process (IInteraction parameters)
+		private IDbCommand GetCommand ()
 		{
 			IDbCommand Command = Connection.CreateCommand ();
 			Command.CommandText = QueryText;
 			Command.Prepare ();
 
-			foreach (string paramname in OrderedParameters) {
-				IDbDataParameter parameter = Command.CreateParameter ();
-				parameter.ParameterName = paramname;
-				parameter.Value = parameters [paramname];
-				Command.Parameters.Add (parameter);
+			return Command;
+		}
+
+		private IDbDataParameter SetCommandParameter (IDbCommand command, string name, string value)
+		{
+			IDbDataParameter parameter = command.CreateParameter();
+			parameter.ParameterName = name;
+			parameter.Value = parameter[name];
+			command.Parameters.Add(parameter);
+		}
+
+		private IDataReader ExecuteParameterizedCommand (IInteraction parameters)
+		{
+			IDbCommand Command = this.GetCommand();
+
+			foreach (string paramname in OrderedParameters) 
+				SetCommandParameter(Command, paramname, parameters[paramname]);
+
+			return Command.ExecuteReader ();
+		}
+
+		private ResultInteraction GetResultInteraction (IDataReader reader, IInteraction parameters, ref int resultCount)
+		{
+			ResultInteraction resultline = none;
+
+			if (reader.Read ()) {
+				resultline = new ResultInteraction(parameters, reader);
+				resultCount++;
 			}
+		}
 
-			IDataReader reader = Command.ExecuteReader();
-			ResultInteraction resultline = null;
-
+		private bool GetResultsToBranches (IInteraction parameters)
+		{			
+			int resultCount = 0;
 			bool success = true;
+			IDataReader 
+				reader = ExecuteParameterizedCommand (parameters);
+			ResultInteraction 
+				firstResult = GetResultInteraction (reader, parameters, ref resultCount),
+			 	nextResult = GetResultInteraction(reader, parameters, ref resultCount);
 
-			while (reader.Read()) 
-			{
-				resultline = new ResultInteraction (parameters, reader);
-				success &= iterator.TryProcess(resultline);
+			if (resultCount == 0) return none.TryProcess (parameters);
+			if ((resultCount == 1) && (single != Stub)) return single.TryProcess(firstResult);
+
+			success &= iterator.TryProcess(firstResult);
+
+			while(resultCount > 1) {
+				success &= iterator.TryProcess(nextResult);
+				resultCount--;
+				nextResult = GetResultInteraction(reader, parameters, ref resultCount);
 			}
-
-			reader.Close();
-
-			if (success && (successful != Stub)) successful.TryProcess(parameters);
 
 			return success;
+		}
+
+		protected override bool Process (IInteraction parameters)
+		{
+			if (GetResultsToBranches(parameters))
+				return successful.TryProcess (parameters);
 		}
 	}
 }
