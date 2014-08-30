f61c2f773dadec6e986c5c79bd79a5a3b19981b1
Rob Tierolff <teh@borreh.nl>
2014-08-30 13:47:05 +0200

    Something changed in the watchablemap. I hope it became more watchable.

--- ModularFunk/Collections/Maps/WatchableMap.cs
+++ ModularFunk/Collections/Maps/WatchableMap.cs
@@ -1,13 +1,20 @@
 using System;
+using System.ComponentModel;
 
 namespace BorrehSoft.Utensils.Collections.Maps
 {
 	/// <summary>
 	/// A regular map, but it raises an event when an item is changed.
 	/// </summary>
-	public class WatchableMap<T> : Map<T>
+	public class WatchableMap<T> : Map<T>, INotifyPropertyChanged
 	{
+		public WatchableMap() : base() {}
+
+		public WatchableMap(Map<T> origin) : base(origin) {}
+
 		public event ItemChangedEventHandler<T> ItemChanged;
+
+		public event PropertyChangedEventHandler PropertyChanged;
 
 		/// <summary>
 		/// Gets or sets the item with the specified name and raises
@@ -24,14 +31,19 @@ namespace BorrehSoft.Utensils.Collections.Maps
 
 				base [name] = value;
 
-				if (ItemChanged == null)
-					return;
+				if (ItemChanged != null)
+					ItemChanged (this, 
+					            new ItemChangedEventArgs<T> (
+						name, PreviousValue, NewValue));
 
-				ItemChanged (this, 
-				            new ItemChangedEventArgs<T> (
-					name, PreviousValue, NewValue));
+				if (PropertyChanged != null)
+					PropertyChanged(this, 
+					                new PropertyChangedEventArgs(
+						name));
+
 			}
 		}
+
 	}
 }
 
