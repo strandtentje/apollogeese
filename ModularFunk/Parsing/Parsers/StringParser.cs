1e10f96b696110102a706e9bd2e63791a66d5f21
Rob Tierolff <teh@borreh.nl>
2014-11-01 15:30:08 +0100

    Found out why empty strings kept on breaking in config files

--- ModularFunk/Parsing/Parsers/StringParser.cs
+++ ModularFunk/Parsing/Parsers/StringParser.cs
@@ -22,21 +22,27 @@ namespace BorrehSoft.Utensils.Parsing.Parsers
 		{
 			// "
 			if (session.Data[session.Offset] == '\"') {
-				string entirity, quotedText, escapedText, text;
+				if (session.Data[session.Offset + 1] != '\"') {
+					string entirity, quotedText, escapedText, text;
 
-				// "cheese"
-				entirity = session.Data.Substring(session.Offset);
+					// "cheese"
+					entirity = session.Data.Substring(session.Offset);
 
-				Match quoteMatch = stringRegex.Match (entirity);
+					Match quoteMatch = stringRegex.Match (entirity);
 
-				quotedText = quoteMatch.Value;
-				escapedText = quotedText.Remove(quotedText.Length - 1, 1).Remove(0, 1);
-				text = Regex.Unescape(escapedText);
+					quotedText = quoteMatch.Value;
+					escapedText = quotedText.Remove(quotedText.Length - 1, 1).Remove(0, 1);
+					text = Regex.Unescape(escapedText);
 
-				session.Offset += quotedText.Length;
+					session.Offset += quotedText.Length;
 
-				result = text;
-				return text.Length;
+					result = text;
+					return text.Length;
+				} else {
+					session.Offset += 2;
+					result = "";
+					return 1;
+				}
 			}
 
 			result = null;
