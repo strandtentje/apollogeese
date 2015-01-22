f78aa9decc102a7d2f4f1688fec48cbd8da2f00a
Rob Tierolff <teh@borreh.nl>
2015-01-22 14:01:03 +0100

    Parsing session registers source file, if applicable

--- Utensils/Parsing/ParsingSession.cs
+++ Utensils/Parsing/ParsingSession.cs
@@ -139,7 +139,19 @@ namespace BorrehSoft.Utensils.Parsing
 		/// <param name="whitespaceParser">Whitespace parser.</param>
 		public static ParsingSession FromFile(string file, Parser whitespaceParser, bool profilingEnabled = false)
 		{
-			return new ParsingSession (File.ReadAllText (file), whitespaceParser, profilingEnabled);
+			ParsingSession result = new ParsingSession (File.ReadAllText (file), whitespaceParser, profilingEnabled);
+			result.SourceFile = new FileInfo (file);
+
+			return result;
+		}
+
+		/// <summary>
+		/// Gets the source file.
+		/// </summary>
+		/// <value>The source file.</value>
+		public FileInfo SourceFile {
+			get;
+			private set;
 		}
 
 		/// <summary>
