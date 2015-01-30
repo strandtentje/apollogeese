6e8810af8c5febd1c4c3816e95f8c6fa414bb9d8
Rob Tierolff <teh@borreh.nl>
2015-01-30 18:02:01 +0100

    Jump Interaction may now reassign context mappings to other names

--- BasicWeblings/Module/JumpInteraction.cs
+++ BasicWeblings/Module/JumpInteraction.cs
@@ -2,6 +2,7 @@ using System;
 using BorrehSoft.ApolloGeese.Duckling;
 using BorrehSoft.Utensils.Collections;
 using BorrehSoft.Utensils.Collections.Settings;
+using System.Collections.Generic;
 
 namespace BorrehSoft.Extensions.BasicWeblings
 {
@@ -9,10 +10,22 @@ namespace BorrehSoft.Extensions.BasicWeblings
 	{
 		public Map<Service> Branches { get; private set; }
 
+
 		public JumpInteraction(IInteraction parent, Map<Service> branches, Settings modSettings) : base(parent, modSettings)
 		{
 			this.Branches = branches;
+
+			object reassignObject = modSettings["reassignments"];
+
+			if (reassignObject != null) {
+				Settings reassignments = (Settings)reassignObject;
+				object value;
+				foreach (KeyValuePair<string, object> pair in reassignments.Dictionary) {
+					string sourceName = pair.Value as string;
+					if (parent.TryGetFallback (sourceName, out value)) 
+						this [pair.Key] = value;
+				}						
+			}
 		}
 	}
 }
-
