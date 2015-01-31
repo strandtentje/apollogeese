ecf0b44e72f82c8879b2112f59801c39b13ce84e
Rob Tierolff <teh@borreh.nl>
2015-01-31 17:12:14 +0100

    Loader documentation

--- Loader/InstanceLoader.cs
+++ Loader/InstanceLoader.cs
@@ -12,8 +12,17 @@ namespace BorrehSoft.ApolloGeese.Loader
 	/// </summary>
 	public static class InstanceLoader
 	{
+		/// <summary>
+		/// Cached caches of module files.
+		/// </summary>
 		private static Map<CachedInstances> cache = new Map<CachedInstances>();
 
+		/// <summary>
+		/// Gets the service instances for a module file.
+		/// </summary>
+		/// <returns>The instances.</returns>
+		/// <param name="file">File.</param>
+		/// <param name="loadPlugins">If set to <c>true</c> load plugins.</param>
 		public static Map <Service> GetInstances(string file, bool loadPlugins = false)
 		{
 			FileInfo info = new FileInfo (file);
@@ -37,6 +46,12 @@ namespace BorrehSoft.ApolloGeese.Loader
 			return entry.Instances;
 		}
 
+		/// <summary>
+		/// Produces a new cache of instantiated services for a certain module file.
+		/// </summary>
+		/// <returns>The new instances.</returns>
+		/// <param name="info">Info.</param>
+		/// <param name="loadPlugins">If set to <c>true</c> load plugins.</param>
 		private static CachedInstances GetNewInstances(FileInfo info, bool loadPlugins)
 		{
 			return new CachedInstances (
