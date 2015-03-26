1f7c33ed11d718fcfe9d7dc9615b533bb28d9906
strandtentje <verkeerslicht@gmail.com>
2015-03-26 14:10:41 +0100

    Started working on discrete HTTP server that won't rely on Windows-supplied HTTP-server.

---
+++ DiscreteHttpServer/HttpInteraction.cs
@@ -0,0 +1,14 @@
+﻿using BorrehSoft.ApolloGeese.Duckling;
+using BorrehSoft.ApolloGeese.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiscreteHttpServer
+{
+    class HttpInteraction : QuickInteraction, IHttpInteraction
+    {
+        
+    }
+}
---
+++ DiscreteHttpServer/HttpServer.cs
@@ -0,0 +1,41 @@
+﻿using BorrehSoft.ApolloGeese.Duckling;
+using BorrehSoft.Utensils.Collections.Maps;
+using BorrehSoft.Utensils.Collections.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscreteHttpServer
+{
+    public class HttpServer : Service
+    {
+        private static prefixPoolInstance = null;
+
+        private static PrefixPool Pool
+        {
+
+        }
+
+        public override string Description
+        {
+            get { return "Discrete http server on TCP socket"; }
+        }
+
+        protected override void HandleBranchChanged(object sender, ItemChangedEventArgs<Service> e)
+        {
+
+        }
+
+        protected override void Initialize(Settings modSettings)
+        {
+            
+        }
+
+        protected override bool Process(IInteraction parameters)
+        {
+            return true;
+        }
+    }
+}
