d71ed0908e71a998e0469e5b2cae6d11fbc8066d
unknown <Rob Tierolff@RITENSE-55.ritense.local>
2015-04-01 17:16:48 +0200

    Cleaned up requestline parser

---
+++ DiscreteHttpServer/RequestVersion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiscreteHttpServer
+{
+    class RequestVersion
+    {
+        internal static RequestVersion FromStream(System.IO.Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
