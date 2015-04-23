13eb94cd14b7b4dfcebde8ca7304a767bf096ef3
Rob Tierolff <teh@borreh.nl>
2015-04-23 21:07:17 +0200

    Who uses LINQ anyways

--- InputProcessing/HtmlReader.cs
+++ InputProcessing/HtmlReader.cs
@@ -1,7 +1,6 @@
 ﻿using BorrehSoft.ApolloGeese.Duckling;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using CsQuery;
 using BorrehSoft.Utensils.Collections.Settings;
