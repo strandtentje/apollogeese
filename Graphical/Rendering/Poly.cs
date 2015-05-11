d5a2c9ec95f89fbcfc480d1c755f216f2e1d43ff
Rob Tierolff <teh@borreh.nl>
2015-05-11 19:53:37 +0200

    Revert "Started work on default vertex"
    This reverts commit 8c9cbdd60430f584d183bcca68cc9a9ccf509e4b.

--- Graphical/Rendering/Poly.cs
+++ Graphical/Rendering/Poly.cs
@@ -8,7 +8,6 @@ using System.Text;
 using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
-using System.Drawing;
 
 namespace Graphical
 {
@@ -22,24 +21,12 @@ namespace Graphical
         
         protected override void Initialize(Settings modSettings)
         {
-            if (modSettings.Has("vertices"))
-            {
-                object vertexObjectListObject = modSettings.Get("vertices");
-                IEnumerable<object> vertexObjectList = (IEnumerable<object>)vertexObjectListObject;
-                vertices = new VertexList();
+            object vertexObjectListObject = modSettings.Get("vertices");
+            IEnumerable<object> vertexObjectList = (IEnumerable<object>)vertexObjectListObject;
+            vertices = new VertexList();
 
-                foreach (object vertexObject in vertexObjectList)
-                    vertices.Add((Settings)vertexObject);
-            }
-            else
-            {
-                string colorName = modSettings.GetString("color", "white");
-                Color4 color = new Color4(Color.FromName(colorName));
-                Vector2 vector = new Vector2(-0.5f, -0.5f);
-                Vertex newVertex = new Vertex(color, vector);
-                vertices.Add(newVertex);
-            }
-
+            foreach (object vertexObject in vertexObjectList)            
+                vertices.Add((Settings)vertexObject);
 
             this.name = modSettings.GetString("name", (shapecount++).ToString());
         }
