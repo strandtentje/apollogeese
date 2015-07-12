6006ea643eb1b78d0cc58c48aa643531daa36b8d
Rob Tierolff <teh@borreh.nl>
2015-07-12 16:23:38 +0200

    Started work on MakePassword-module. Probably prone to renaming later.

---
+++ Auth/MakePassword.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BorrehSoft.ApolloGeese.Duckling;
+using BorrehSoft.Utensils.Collections.Settings;
+using BorrehSoft.Utensils.Collections.Maps;
+
+namespace Auth
+{
+	public class MakePassword : Service
+	{
+		public override string Description {
+			get {
+				return "Password hasher";
+			}
+		}
+
+		protected override void Initialize (Settings modSettings)
+		{
+
+		}
+
+		protected override void HandleBranchChanged (object sender, ItemChangedEventArgs<Service> e)
+		{
+
+		}
+
+		protected override bool Process (IInteraction parameters)
+		{
+			return false;
+		}
+	}
+}
+
