e5475aef34e19453edeafae3d6b3dd4203a1aa51
Rob Tierolff <teh@borreh.nl>
2015-07-12 17:27:25 +0200

    Coupling in assignment now happens in seperate function

--- Utensils/Parsing/Parsers/AssignmentParser.cs
+++ Utensils/Parsing/Parsers/AssignmentParser.cs
@@ -19,16 +19,21 @@ namespace BorrehSoft.Utensils.Parsing.Parsers
 			return "Assignment";
 		}
 
+		protected virtual bool Couple(ParsingSession session, ref object identifier) {
+			return coupler.Run (session) > -1;
+		}
+
 		internal override int ParseMethod (ParsingSession session, out object result)
 		{
 			object identifier, value;
+			int returnValue = -1;
 
 			result = null;
 
 			// dick
 			if (identifierParser.Run (session, out identifier) > -1) {
 				// = 
-				if (coupler.Run (session) > -1) {
+				if (Couple(session, ref identifier)) {
 					// "butt";
 					session.DeepenContext(identifier as String);
 					int iresult = InnerParser.Run (session, out value);
@@ -36,14 +41,14 @@ namespace BorrehSoft.Utensils.Parsing.Parsers
 						session.ContextRegister(value);
 						session.SurfaceContext(identifier as String);
 						result = new Tuple<string, object> ((string)identifier, value);
-						return 1;
+						returnValue = 1;
 					} else {
 						session.SurfaceContext(identifier as String);
 					}
 				} 
 			}
 
-			return -1;
+			return returnValue;
 		}
 	}
 }
