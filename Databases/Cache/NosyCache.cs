8c52ada55250797c2abbeae32552a0b179476ad0
Rob Tierolff <teh@borreh.nl>
2015-07-17 23:19:54 +0200

    Nosy cache that uses NosyInteractions for acquiring caching keys.

---
+++ Databases/Cache/NosyCache.cs
@@ -0,0 +1,33 @@
+using System;
+using BorrehSoft.ApolloGeese.Duckling;
+using BorrehSoft.Utensils.Collections.Settings;
+
+namespace BorrehSoft.ApolloGeese.Extensions.Data.Cache
+{
+	public class NosyCache : ContextCache
+	{
+		bool includeContext;
+
+		protected override void Initialize (Settings modSettings)
+		{
+			includeContext = modSettings.GetBool ("includecontext");
+
+			base.Initialize (modSettings);
+		}
+
+		protected override string GetCacheName (IInteraction parameters)
+		{
+			NosyInteraction interaction = new NosyInteraction (includeContext, parameters);
+			string cacheName;
+
+			if (BeginBranch.TryProcess (interaction)) {
+				cacheName = interaction.Signature;
+			} else {
+				throw new Exception ("begin-branch of nosy interaction produced false.");
+			}
+
+			return cacheName;
+		}
+	}
+}
+
