ccf94ed1b51d664d1f807118e66c0a503ddf0690
Rob Tierolff <teh@borreh.nl>
2015-07-20 00:45:20 +0200

    FlowView implementation that shows model instead

---
+++ FlowOfOperations/Visualization/ModuleView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BorrehSoft.ApolloGeese.Duckling;
+using MModule = BorrehSoft.ApolloGeese.Extensions.FlowOfOperations.Module.Module;
+
+namespace BorrehSoft.ApolloGeese.Extensions.FlowOfOperations
+{
+	public class ModuleView : FlowView
+	{
+		protected override Service GetModel (IInteraction parameters)
+		{
+			object modelIdObject;
+
+			if (parameters.TryGetFallback ("modelid", out modelIdObject)) {
+				int modelIdInt;
+				if (int.TryParse (modelIdObject.ToString (), out modelIdInt)) {
+					Service candidate = ModelLookup [modelIdInt];
+					if (candidate is MModule) {
+						MModule module = (MModule)candidate;
+						if (module.BranchName != null) {
+							return module.ModuleBranches [module.BranchName];
+						} else {
+							throw new Exception ("Module call requires branch too");
+						}
+					} else {
+						throw new Exception ("This service isn't a module");
+					}
+				} else {
+					throw new Exception ("Expected model id");
+				}
+			} else {
+				throw new Exception ("Value of\t modelid not set");
+			}
+		}
+	}
+}
+
