7c5d06d9c9ed9792295528e7ae3d11851e62a12a
Rob Tierolff <teh@borreh.nl>
2015-07-24 20:16:58 +0200

    Tabbing  for readability

--- TestSuite/Program.cs
+++ TestSuite/Program.cs
@@ -22,20 +22,20 @@ namespace TestSuite
 
 			IEnumerable<string> plugins = new string[] { "/usr/lib/BasicHttpServer.dll", "/usr/lib/OutputComposing.dll" };
 			data ["plugins"] = plugins;
-			Settings instances = new Settings ();
-			Settings firstInstance = new Settings ();
-			firstInstance ["type"] = "HttpService";
-			Settings modConf = new Settings ();
-			IEnumerable<string> prefixes = new string[] { "http://localhost:8080/" };
-			modConf ["prefixes"] = prefixes;
-			firstInstance ["modconf"] = modConf;
-			Settings underlyingInstance = new Settings ();
-			underlyingInstance ["type"] = "Template";
-			Settings templateConf = new Settings ();
-			templateConf ["templatefile"] = "/var/www/index.html";
-			underlyingInstance ["modconf"] = templateConf;
-			firstInstance ["http_branch"] = underlyingInstance;
-			instances ["firstinstance"] = firstInstance;
+				Settings instances = new Settings ();
+					Settings firstInstance = new Settings ();
+					firstInstance ["type"] = "HttpService";
+					Settings modConf = new Settings ();
+						IEnumerable<string> prefixes = new string[] { "http://localhost:8080/" };
+						modConf ["prefixes"] = prefixes;
+					firstInstance ["modconf"] = modConf;
+					Settings underlyingInstance = new Settings ();
+						underlyingInstance ["type"] = "Template";
+						Settings templateConf = new Settings ();
+							templateConf ["templatefile"] = "/var/www/index.html";
+						underlyingInstance ["modconf"] = templateConf;
+					firstInstance ["http_branch"] = underlyingInstance;
+				instances ["firstinstance"] = firstInstance;
 			data ["instances"] = instances;
 
 			return composer.Serialize (data);
