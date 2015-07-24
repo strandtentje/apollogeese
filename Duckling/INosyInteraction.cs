e354c7407167e022c9bc5a7ca3a67372d49927b8
Rob Tierolff <teh@borreh.nl>
2015-07-24 17:35:16 +0200

    Docs

--- Duckling/INosyInteraction.cs
+++ Duckling/INosyInteraction.cs
@@ -4,6 +4,10 @@ namespace BorrehSoft.ApolloGeese.Duckling
 {
 	public interface INosyInteraction : IInteraction
 	{
+		/// <summary>
+		/// ???
+		/// </summary>
+		/// <value><c>true</c> if include context; otherwise, <c>false</c>.</value>
 		bool IncludeContext { get; }
 
 		string Signature { get; set; }
