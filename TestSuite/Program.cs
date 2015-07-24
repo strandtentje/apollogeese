2cfb11a3f8121be9e68bc49d26f786eda3a9758d
Rob Tierolff <teh@borreh.nl>
2015-07-24 20:15:22 +0200

    New test suite with rather complicated test case.

--- TestSuite/Program.cs
+++ TestSuite/Program.cs
@@ -1,4 +1,8 @@
 using System;
+using System.IO;
+using BorrehSoft.Utensils;
+using BorrehSoft.Utensils.Collections.Settings;
+using System.Collections.Generic;
 
 namespace TestSuite
 {
@@ -6,7 +10,47 @@ namespace TestSuite
 	{
 		public static void Main (string[] args)
 		{
+			SettingsComposer composer = new SettingsComposer ();
 
+			Console.WriteLine ("Test case 1...");
+			TestAgainst (RunTestcase1(composer), "TestCase1.conf");
+		}
+
+		static string RunTestcase1 (SettingsComposer composer)
+		{
+			Settings data = Settings ();
+
+			IEnumerable<string> plugins = new string[] { "/usr/lib/BasicHttpServer.dll", "/usr/lib/OutputComposing.dll" };
+			data ["plugins"] = plugins;
+			Settings instances = new Settings ();
+			Settings firstInstance = new Settings ();
+			firstInstance ["type"] = "HttpService";
+			Settings modConf = new Settings ();
+			IEnumerable<string> prefixes = new string[] { "http://localhost:8080/" };
+			modConf ["prefixes"] = prefixes;
+			firstInstance ["modconf"] = modConf;
+			Settings underlyingInstance = new Settings ();
+			underlyingInstance ["type"] = "Template";
+			Settings templateConf = new Settings ();
+			templateConf ["templatefile"] = "/var/www/index.html";
+			underlyingInstance ["modconf"] = templateConf;
+			firstInstance ["http_branch"] = underlyingInstance;
+			instances ["firstinstance"] = firstInstance;
+			data ["instances"] = instances;
+
+			return composer.Serialize (data);
+		}
+
+		public void TestAgainst(string data, string verifile) {			
+			using (StreamReader reader = new StreamReader(File.OpenRead(verifile))) {
+				string verificationData = reader.ReadToEnd ();
+
+				if (data == verificationData) {
+					Console.WriteLine ("All is good");
+				} else {
+					Console.WriteLine ("Burp");
+				}
+			}
 		}
 	}
 }
