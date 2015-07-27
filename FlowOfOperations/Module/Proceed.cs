a94104414c8c38d2a78f966fbe2716800f3e2a95
Rob Tierolff <teh@borreh.nl>
2015-07-27 22:29:25 +0200

    Proceed service that behaves like return, service, but flags FlowViewer that this service should be skipped in favour of an alternative service.

---
+++ FlowOfOperations/Module/Proceed.cs
@@ -0,0 +1,11 @@
+using System;
+using BorrehSoft.ApolloGeese.Extensions.FlowOfOperations.Module;
+
+namespace BorrehSoft.ApolloGeese.Extensions.FlowOfOperations
+{
+	public class Proceed : Return
+	{
+
+	}
+}
+
