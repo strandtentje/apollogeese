e331374eabbad7b0c1e4e5c65845a2e58bcafd6d
Rob Tierolff <teh@borreh.nl>
2015-07-27 22:54:49 +0200

    SiteSubsection interaction now allows recalling of URL at current context level.

--- Navigation/SubsectionInteraction.cs
+++ Navigation/SubsectionInteraction.cs
@@ -3,6 +3,7 @@ using BorrehSoft.ApolloGeese.Duckling;
 using BorrehSoft.Utensils.Collections.Settings;
 using BorrehSoft.ApolloGeese.Http;
 using BorrehSoft.Utensils.Collections.Maps;
+using System.Text;
 
 namespace BorrehSoft.ApolloGeese.Extensions.Navigation
 {
@@ -23,6 +24,31 @@ namespace BorrehSoft.ApolloGeese.Extensions.Navigation
 		/// <value><c>true</c> if this instance has more 'directory'-names; otherwise, <c>false</c>.</value>
 		public bool HasTail { get; private set; }
 
+		private void WriteCurrentUrl(StringBuilder builder)
+		{
+			IInteraction closestKinInteraction;
+
+			if (this.Parent != null) {
+				if (this.Parent.TryGetClosest (typeof(SubsectionInteraction), out closestKinInteraction)) {
+					SubsectionInteraction closestKin = (SubsectionInteraction)closestKinInteraction;
+
+					closestKin.WriteCurrentUrl (builder);
+				}
+			}
+
+			if (this.BranchName != "main")
+				builder.AppendFormat ("/{0}", this.BranchName);
+		}
+
+		private string FindCurrentUrl ()
+		{
+			StringBuilder urlBuilder = new StringBuilder ();
+
+			WriteCurrentUrl (urlBuilder);
+
+			return urlBuilder.ToString ();
+		}
+
 		/// <summary>
 		/// Initializes a new instance of the <see cref="BorrehSoft.Extensions.Navigation.SubsectionInteraction"/> class.
 		/// </summary>
@@ -38,6 +64,8 @@ namespace BorrehSoft.ApolloGeese.Extensions.Navigation
 
 			if (HasTail) 
 				this ["branchname"] = http.URL.Peek ();
+
+			this ["currenturl"] = FindCurrentUrl ();
 		}
 
 		/// <summary>
