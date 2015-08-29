8a71fabb81bdf5b7ab03f5e406bf149e4181d1ba
Rob Tierolff <teh@borreh.nl>
2015-08-29 02:50:57 +0200

    HttpStatus allows setting of MIME types, temporarily.

--- BasicHttpServer/HttpStatus.cs
+++ BasicHttpServer/HttpStatus.cs
@@ -2,6 +2,7 @@ using System;
 using BorrehSoft.ApolloGeese.CoreTypes;
 using BorrehSoft.Utensils.Collections.Settings;
 using BorrehSoft.Utensils.Collections.Maps;
+using BorrehSoft.ApolloGeese.Http.Headers;
 
 namespace BorrehSoft.ApolloGeese.Extensions.BasicHttpServer
 {
@@ -15,6 +16,11 @@ namespace BorrehSoft.ApolloGeese.Extensions.BasicHttpServer
 
 		[Instruction("HTTP status code to produce", 200)]
 		public int Statuscode { get; set; }
+
+		string MimeType {
+			get;
+			set;
+		}
 
 		private Service nextService;
 
@@ -33,12 +39,17 @@ namespace BorrehSoft.ApolloGeese.Extensions.BasicHttpServer
 		protected override void Initialize (Settings modSettings)
 		{
 			this.Statuscode = modSettings.GetInt ("statuscode", 200);
+			this.MimeType = modSettings.GetString ("mime", "");
 		}
 
 		protected override bool Process (IInteraction parameters)
 		{
 			HttpInteraction interaction = (HttpInteraction)parameters.GetClosest (typeof(HttpInteraction));
 			interaction.SetStatuscode (this.Statuscode);
+
+			if (MimeType.Length > 0) {
+				interaction.ResponseHeaders.ContentType = new MimeType (this.MimeType);
+			}
 
 			return nextService.TryProcess(parameters);
 		}
