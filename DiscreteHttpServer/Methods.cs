91616d0ac6dd54d8f54486c00ee9e5b8b89d103b
Rob Tierolff <rob.tierolff@ritense.com>
2015-09-10 17:07:22 +0200

    HTTP method enum

---
+++ DiscreteHttpServer/Methods.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DiscreteHttpServer
+{
+	public enum Methods { OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT };
+}
