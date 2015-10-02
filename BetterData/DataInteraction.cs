4d82a33d6c2d3de9160fa980e76c6e22d0de282c
Rob Tierolff <teh@borreh.nl>
2015-10-02 14:35:14 +0200

    Introduced DataInteraction

---
+++ BetterData/DataInteraction.cs
@@ -0,0 +1,17 @@
+using System;
+using BorrehSoft.ApolloGeese.CoreTypes;
+using System.Data;
+using System.Collections.Generic;
+
+namespace BetterData
+{
+	class DataInteraction : SimpleInteraction
+	{
+		public DataInteraction (string[] columnNames, object[] values)
+		{
+
+		}
+	}
+
+}
+
