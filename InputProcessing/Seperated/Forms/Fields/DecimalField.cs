929479c771cdce52c26e50225fa169cd9bd5dece
Rob Tierolff <teh@borreh.nl>
2015-10-16 00:06:06 +0200

    Introduced field for decimal data

---
+++ InputProcessing/Seperated/Forms/Fields/DecimalField.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BorrehSoft.Utensils.Collections.Settings;
+
+namespace InputProcessing
+{
+	/// <summary>
+	/// Decimal field.
+	/// </summary>
+	public class DecimalField : ValueField<decimal>
+	{
+		protected override void Initialize (Settings settings)
+		{
+			base.Initialize (settings);
+
+			this.Default = settings.GetDecimal ("default", 0);
+			this.Min = settings.GetDecimal ("min", decimal.MinValue);
+			this.Max = settings.GetDecimal ("max", decimal.MaxValue);
+		}
+
+		public override bool TryParse (string serial, out decimal data)
+		{
+			return decimal.TryParse (serial, out data);
+		}
+	}
+}
+
