1607d2fa60340d9952b86cce757ed52ee38657a2
Rob Tierolff <teh@borreh.nl>
2015-10-16 00:06:19 +0200

    Started work on text field

---
+++ InputProcessing/Seperated/Forms/Fields/TextField.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BorrehSoft.Utensils.Collections.Settings;
+using BorrehSoft.ApolloGeese.CoreTypes;
+
+namespace InputProcessing
+{
+	public class TextField : Field<string>
+	{
+		protected override void Initialize (Settings settings)
+		{
+
+		}
+
+		override Service FindActionForValue (object valueCandidate, out string value)
+		{
+
+		}
+	}
+}
+
