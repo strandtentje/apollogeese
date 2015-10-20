6355374071729376df5a3b1abc76a08b8eff7610
Rob Tierolff <teh@borreh.nl>
2015-10-20 23:47:43 +0200

    Reintroduced writerinteraction temporarily because its absence broke something

---
+++ CoreTypes/Interactions/IOutgoingWriterInteraction.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+
+namespace BorrehSoft.ApolloGeese.CoreTypes
+{
+	public interface IOutgoingWriterInteraction : IOutgoingBodiedInteraction
+	{		
+		/// <summary>
+		/// Gets a StreamWriter for the ougoing body.
+		/// </summary>
+		/// <returns>The outgoing body writer.</returns>
+		StreamWriter GetOutgoingBodyWriter();
+
+		/// <summary>
+		/// Gets a value indicating whether or not a writer has been produced for the stream
+		/// </summary>
+		/// <returns><c>true</c> if this instance has writer; otherwise, <c>false</c>.</returns>
+		bool HasWriter();
+	}
+}
+
