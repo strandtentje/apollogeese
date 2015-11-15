65af61e0466d1c1d3b88ce5fc203cd35867f3d2f
Rob Tierolff <teh@borreh.nl>
2015-11-15 14:55:34 +0100

    Refacted Cache into anonymouscache, introduced a not-anonymous cache.

---
+++ Databases/Cache/AnonymousCache.cs
@@ -0,0 +1,98 @@
+using System;
+using BorrehSoft.ApolloGeese.CoreTypes;
+using BorrehSoft.Utensils.Collections.Settings;
+using BorrehSoft.Utensils.Collections.Maps;
+using System.IO;
+
+namespace BorrehSoft.ApolloGeese.Extensions.Data.Cache
+{
+	public class AnonymousCache : Service
+	{
+		public override string Description {
+			get {
+				return "Caches IOutgoingInteractions";
+			}
+		}
+
+		[Instruction("Lifetime of Cache.")]
+		public string CacheLifetime { 
+			get {
+				if (timeUntilCacheDrop == TimeSpan.MaxValue)
+					return "";
+				else
+					return timeUntilCacheDrop.ToString ();
+			}
+			set {
+				if (value.Length == 0)
+					timeUntilCacheDrop = TimeSpan.MaxValue;
+				else
+					timeUntilCacheDrop = TimeSpan.Parse (value);
+			}
+		}
+
+		TimeSpan timeUntilCacheDrop;
+		DateTime lastUpdate = DateTime.Now;
+
+		public override void LoadDefaultParameters (string defaultParameter)
+		{
+			this.Settings ["lifetime"] = defaultParameter;
+		}
+
+		protected override void Initialize (Settings modSettings)
+		{
+			this.CacheLifetime = modSettings.GetString ("lifetime", "");
+			this.Data = null;
+		}
+
+		private Service begin;
+
+		protected virtual byte[] Data { get; set; }
+
+		protected override void HandleBranchChanged (object sender, ItemChangedEventArgs<Service> e)
+		{
+			if (e.Name == "begin")
+				begin = e.NewValue;
+		}
+	
+		protected override bool Process (IInteraction parameters)
+		{
+			bool success = true;
+			IOutgoingBodiedInteraction upstreamTarget;
+			upstreamTarget = (IOutgoingBodiedInteraction)parameters.GetClosest (
+				typeof(IOutgoingBodiedInteraction));
+
+			if (timeUntilCacheDrop != TimeSpan.MaxValue) {
+				if (DateTime.Now - lastUpdate > timeUntilCacheDrop) {
+					Data = null;
+				}
+			}
+
+			if (Data == null) {
+				MemoryStream targetStream = new MemoryStream();
+				SimpleOutgoingInteraction downstreamTarget = new SimpleOutgoingInteraction (
+					targetStream, upstreamTarget.Encoding, parameters);
+
+				success = begin.TryProcess (downstreamTarget);		
+				downstreamTarget.Done ();
+
+				targetStream.Position = 0;
+
+				if (downstreamTarget.HasWriter()) {
+					using (StreamReader reader = new StreamReader(targetStream)) 
+						Data = downstreamTarget.Encoding.GetBytes (reader.ReadToEnd ());
+				} else {
+					Data = new byte[targetStream.Length];
+					targetStream.Read (Data, 0, Data.Length);
+					targetStream.Dispose ();
+				}
+
+				lastUpdate = DateTime.Now;
+			} 
+
+			upstreamTarget.OutgoingBody.Write (Data, 0, Data.Length);
+
+			return success;
+		}
+	}
+}
+
--- Databases/Cache/Cache.cs
+++ Databases/Cache/Cache.cs
@@ -1,100 +1,37 @@
 using System;
-using BorrehSoft.ApolloGeese.CoreTypes;
 using BorrehSoft.Utensils.Collections.Settings;
-using BorrehSoft.Utensils.Collections.Maps;
-using System.IO;
+using System.Collections.Generic;
+using BorrehSoft.Utensils.Collections;
 
 namespace BorrehSoft.ApolloGeese.Extensions.Data.Cache
 {
-	public class Cache : Service
+	public class Cache : AnonymousCache
 	{
-		public override string Description {
-			get {
-				return "Caches IOutgoingInteractions";
-			}
-		}
-
-		[Instruction("Lifetime of Cache.")]
-		public string CacheLifetime { 
-			get {
-				if (timeUntilCacheDrop == TimeSpan.MaxValue)
-					return "";
-				else
-					return timeUntilCacheDrop.ToString ();
-			}
-			set {
-				if (value.Length == 0)
-					timeUntilCacheDrop = TimeSpan.MaxValue;
-				else
-					timeUntilCacheDrop = TimeSpan.Parse (value);
-			}
-		}
-
-		TimeSpan timeUntilCacheDrop;
-		DateTime lastUpdate = DateTime.Now;
-
 		public override void LoadDefaultParameters (string defaultParameter)
 		{
-			this.Settings ["lifetime"] = defaultParameter;
+			Settings ["name"] = defaultParameter;
+		}
+
+		string Name {
+			get;
+			set;
 		}
 
 		protected override void Initialize (Settings modSettings)
 		{
-			this.CacheLifetime = modSettings.GetString ("lifetime", "");
+			this.Name = modSettings.GetString ("name");
+			base.Initialize (modSettings);
 		}
 
-		private Service begin;
-		private bool isStringCache;
+		private Map<byte[]> storage = new Map<byte[]> ();
 
-		protected virtual byte[] binaryData { get; set; }
-		protected virtual string stringData { get; set; }
-
-		protected override void HandleBranchChanged (object sender, ItemChangedEventArgs<Service> e)
-		{
-			if (e.Name == "begin")
-				begin = e.NewValue;
-		}
-	
-		protected override bool Process (IInteraction parameters)
-		{
-			bool success = true;
-			IOutgoingBodiedInteraction upstreamTarget;
-			upstreamTarget = (IOutgoingBodiedInteraction)parameters.GetClosest (
-				typeof(IOutgoingBodiedInteraction));
-
-			if (timeUntilCacheDrop != TimeSpan.MaxValue) {
-				if (DateTime.Now - lastUpdate > timeUntilCacheDrop) {
-					binaryData = null;
-					stringData = null;
-				}
+		protected override byte[] Data {
+			get {
+				return storage [this.Name];
 			}
-
-			if ((binaryData == null) && (stringData == null)) {
-				MemoryStream targetStream = new MemoryStream();
-				SimpleOutgoingInteraction downstreamTarget = new SimpleOutgoingInteraction (
-					targetStream, upstreamTarget.Encoding, parameters);
-
-				success = begin.TryProcess (downstreamTarget);		
-				downstreamTarget.Done ();
-
-				targetStream.Position = 0;
-
-				if (downstreamTarget.HasWriter()) {
-					isStringCache = true;
-					using (StreamReader reader = new StreamReader(targetStream)) 
-						stringData = reader.ReadToEnd ();
-				} else {
-					binaryData = new byte[targetStream.Length];
-					targetStream.Read (binaryData, 0, binaryData.Length);
-					targetStream.Dispose ();
-				}
-
-				lastUpdate = DateTime.Now;
-			} 
-
-			upstreamTarget.OutgoingBody.Write (binaryData, 0, binaryData.Length);
-
-			return success;
+			set {
+				storage [this.Name] = value;
+			}
 		}
 	}
 }
