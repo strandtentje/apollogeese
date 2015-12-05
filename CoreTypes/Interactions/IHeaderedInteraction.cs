7beaf64740ede32b9ee86ba7f3e893512fcbabbd
Rob Tierolff <teh@borreh.nl>
2015-12-05 14:18:35 +0100

    Introduced headered interaction interface

---
+++ CoreTypes/Interactions/IHeaderedInteraction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Specialized;
+
+namespace BorrehSoft.ApolloGeese.CoreTypes
+{
+	public interface IHeaderedInteraction
+	{
+
+		/// <summary>
+		/// Gets the request headers.
+		/// </summary>
+		/// <value>The request headers.</value>
+		NameValueCollection RequestHeaders { get; }
+
+		/// <summary>
+		/// Gets the response headers.
+		/// </summary>
+		/// <value>The response headers.</value>
+		NameValueCollection ResponseHeaders { get; }
+
+	}
+}
+
