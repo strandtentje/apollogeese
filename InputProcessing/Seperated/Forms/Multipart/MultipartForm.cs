290872fab485e5ffc3d1a947476aae13069e176a
Rob Tierolff <teh@borreh.nl>
2015-12-05 13:25:28 +0100

    Started work on multipart forms (again?)

---
+++ InputProcessing/Seperated/Forms/Multipart/MultipartForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using BorrehSoft.ApolloGeese.CoreTypes;
+using BorrehSoft.ApolloGeese.Http;
+
+namespace InputProcessing
+{
+	public class MultipartForm : Form
+	{
+		protected override IRawInputInteraction GetReader (IInteraction parameters)
+		{
+			IInteraction ancestor;
+			IHttpInteraction httpAncestor;
+
+			if (parameters.TryGetClosest (typeof(IHttpInteraction), out ancestor)) {
+				httpAncestor = (IHttpInteraction)ancestor;
+
+				// httpAncestor.
+			} else {
+				throw new Exception ("Multipart form only works in http context.");
+			}
+		}
+	}
+}
+
