4657604f3b3e73bc9f70282c054f7d89d4e1c29d
Rob Tierolff <rob.tierolff@ritense.com>
2015-12-08 10:06:22 +0100

    Introduced XmlDocument wrapper

---
+++ Databases/XMLNodes.cs
@@ -0,0 +1,84 @@
+﻿using BorrehSoft.ApolloGeese.CoreTypes;
+using BorrehSoft.Utensils.Collections.Maps;
+using BorrehSoft.Utensils.Collections.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Data
+{
+    class XML : Service
+    {
+        public override string Description
+        {
+            get { return "XML Iterator"; }
+        }
+
+        protected override void Initialize(Settings settings)
+        {
+            this.DefaultBranch = settings.GetString("defaultbranch", "default");
+            this.TextBranch = settings.GetString("textbranch", "text");
+            this.TextVariable = settings.GetString("textvariable", "text");
+        }
+
+        protected override void HandleBranchChanged(object sender, ItemChangedEventArgs<Service> e)
+        {
+            
+        }
+
+
+
+        protected override bool Process(IInteraction parameters)
+        {
+            XmlNodeInteraction preceeding = FindNode(parameters);
+            string name;
+            bool successful = true;
+
+            foreach (XmlNode child in preceeding.Node.ChildNodes)
+            {
+                name = child.LocalName.TrimStart('#');
+
+                if (Branches.Has(name))
+                {
+                    successful &= Branches[name].TryProcess(new XmlNodeInteraction(parameters, child));
+                }
+            }
+
+            return true;
+        }
+
+        private XmlNodeInteraction FindNode(IInteraction parameters)
+        {
+            IInteraction candidate;
+
+            if (parameters.TryGetClosest(typeof(XmlNodeInteraction), out candidate))
+            {
+                XmlNodeInteraction xmlNodeInteraction = (XmlNodeInteraction)candidate;
+
+                return xmlNodeInteraction;
+            }
+            else if (parameters.TryGetClosest(typeof(IIncomingBodiedInteraction), out candidate))
+            {
+                IIncomingBodiedInteraction source = (IIncomingBodiedInteraction)candidate;
+
+                XmlDocument document = new XmlDocument();
+
+                document.Load(source.IncomingBody);
+
+                return new XmlNodeInteraction(parameters, document);
+            }
+            else
+            {
+                throw new Exception("Couldn't find or produce XML document");
+            }
+        }
+
+        public string DefaultBranch { get; set; }
+
+        public string TextBranch { get; set; }
+
+        public string TextVariable { get; set; }
+    }
+}
---
+++ Databases/XmlAttributeInteraction.cs
@@ -0,0 +1,45 @@
+﻿using BorrehSoft.ApolloGeese.CoreTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Data
+{
+    class XmlNodeInteraction : SimpleInteraction
+    {
+        private IInteraction parameters;
+        public XmlNode Node { get; private set; }
+
+        public XmlNodeInteraction(IInteraction parameters, XmlNode node) : base(parameters)
+        {   
+            this.Node = node;
+
+            if (node.Attributes != null)
+            {
+                foreach (XmlAttribute attrib in node.Attributes)
+                {
+                    this[attrib.Name] = attrib.Value;
+                }
+            }            
+        }
+
+        public override bool Has(string key)
+        {
+            return (key == "node_text") || base.Has(key);
+        }
+
+        public override object Get(string key)
+        {
+            if (key == "node_text")
+            {
+                return this.Node.InnerText;
+            }
+            else
+            {
+                return base.Get(key);
+            }            
+        }
+    }
+}
