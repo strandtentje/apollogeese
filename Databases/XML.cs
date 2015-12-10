d6d26efee70662c36b4708ba0aaf86606f933ab4
Rob Tierolff <teh@borreh.nl>
2015-12-10 23:49:28 +0100

    These dependencies in the XML modules were what one'd call in dutch 'nodeloos'

--- Databases/XML.cs
+++ Databases/XML.cs
@@ -3,7 +3,6 @@ using BorrehSoft.Utensils.Collections.Maps;
 using BorrehSoft.Utensils.Collections.Settings;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Xml;
 
--- Databases/XmlNodeInteraction.cs
+++ Databases/XmlNodeInteraction.cs
@@ -1,7 +1,6 @@
 ﻿using BorrehSoft.ApolloGeese.CoreTypes;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Xml;
 
