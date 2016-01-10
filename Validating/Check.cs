4c0616b862f03e3c40e0b671adaf35d960ec3c92
Rob Tierolfff <teh@borreh.nl>
2016-01-11 00:29:45 +0100

    Validation apparatus that doesn't work but switching IDE's because this one seems broken

---
+++ Validating/Check.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using BorrehSoft.ApolloGeese.CoreTypes;
+using BorrehSoft.Utensils.Collections.Settings;
+using BorrehSoft.Utensils.Collections.Maps;
+
+namespace Validating
+{
+	public class Check : TwoBranchedService
+	{
+		public override string Description {
+			get {
+				return "Branch either successful or failure depending on whether or" +
+				"not failures were reported downstream on 'subject'";
+			}
+		}
+
+		protected override void Initialize (Settings settings)
+		{
+			
+		}
+
+		public Service Subject { get; set; }
+
+		protected override void HandleBranchChanged (object sender, ItemChangedEventArgs<Service> e)
+		{
+			base.HandleBranchChanged (sender, e);
+
+			if (e.Name == "subject") {
+				this.Subject = e.NewValue;
+			}
+		}
+
+		protected override bool Process (IInteraction parameters)
+		{
+			CheckInteraction parameters = new CheckInteraction(parameters);
+
+			bool success;
+
+			if (this.Subject.TryProcess(parameters) && parameters.Successful)  {
+				success &= this.Successful.TryProcess(parameters);
+			} else {
+				success &= this.Failure.TryProcess(parameters);
+			}
+
+			return success;
+		}
+	}
+}
+
---
+++ Validating/CheckInteraction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BorrehSoft.ApolloGeese.CoreTypes;
+
+namespace Validating
+{
+	public class CheckInteraction : BareInteraction
+	{
+		public CheckInteraction (IInteraction parent) : base(parent)
+		{
+			this.Successful = true;
+		}	
+
+		public bool Successful { get; set; }
+	}
+}
+
