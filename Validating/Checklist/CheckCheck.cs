ea3e31c742a2df66c28cfa2baf385cd42560ea40
Rob Tierolff <teh@borreh.nl>
2016-02-17 19:29:55 +0100

    Added CheckCheck to Check the Check state

---
+++ Validating/Checklist/CheckCheck.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using BorrehSoft.ApolloGeese.CoreTypes;
+using BorrehSoft.Utensils.Collections.Settings;
+using BorrehSoft.Utensils.Collections.Maps;
+
+namespace Validating
+{
+	public class CheckCheck : TwoBranchedService
+	{
+		public override string Description {
+			get {
+				return "Branch successful if Check-session was succesful" +
+				" thus far";
+			}
+		}
+
+		protected override void Initialize (Settings settings)
+		{
+			
+		}
+
+		protected override bool Process (IInteraction parameters)
+		{
+			IInteraction candidate;
+			bool successful;
+
+			if (successful = parameters.TryGetClosest (typeof(CheckInteraction), out candidate)) {
+				CheckInteraction callback = (CheckInteraction)candidate;
+				if (callback.Successful) {
+					successful = this.Successful.TryProcess (parameters);
+				} else {
+					successful = this.Failure.TryProcess (parameters);
+				}
+			}
+
+			return successful;
+		}
+	}
+}
+
