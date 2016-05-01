e5d7204593560e92cfa96a170669e831d04fd4d0
Rob Tierolff <teh@borreh.nl>
2016-05-02 00:12:44 +0200

    Extended settingsview to allow map iteration

--- FlowOfOperations/Reflection/View/SettingsView.cs
+++ FlowOfOperations/Reflection/View/SettingsView.cs
@@ -15,9 +15,19 @@ namespace BorrehSoft.ApolloGeese.Extensions.FlowOfOperations
 			}
 		}
 
+		Settings sourceSettings = null;
+		string sourceSettingsVariable = null;
+
 		protected override void Initialize (Settings modSettings)
 		{
+			sourceSettings = null;
+			sourceSettingsVariable = null;
 
+			if (modSettings.Has ("sourcesettings")) {
+				this.sourceSettings = modSettings.GetSubsettings ("sourcesettings");
+			} else if (modSettings.Has ("sourcesettingsvariable")) {
+				this.sourceSettingsVariable = modSettings.GetString ("sourcesettigsvariable");
+			}
 		}
 
 		Service None { get; set; }
@@ -47,7 +57,20 @@ namespace BorrehSoft.ApolloGeese.Extensions.FlowOfOperations
 		{
 			object modelIdObject;
 
-			if (parameters.TryGetFallback ("serviceid", out modelIdObject)) {
+			if (sourceSettings != null) {
+				ViewMap (sourceSettings, parameters);
+			} else if (sourceSettingsVariable != null) {
+				object contextualSourceSettings;
+				if (parameters.TryGetFallback (this.sourceSettingsVariable, out contextualSourceSettings)) {
+					if (contextualSourceSettings is Map<object>) {
+						ViewMap ((Map<object>)contextualSourceSettings, parameters);
+					} else {
+						throw new Exception (string.Format ("Expected map in context at {0}", this.sourceSettingsVariable));
+					}
+				} else {
+					throw new Exception (string.Format ("Expected something at {0}", this.sourceSettingsVariable));
+				}
+			} else if (parameters.TryGetFallback ("serviceid", out modelIdObject)) {
 				int modelIdInt;
 				if (int.TryParse (modelIdObject.ToString (), out modelIdInt)) {
 					Service candidate = ModelLookup [modelIdInt];
