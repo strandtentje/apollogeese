eddac9326195f4c3d7b498b0cfcd0e56cec21c8a
Rob Tierolff <teh@borreh.nl>
2016-05-26 01:24:45 +0200

    Datefield

---
+++ Validating/Static/DateField.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using BorrehSoft.Utensils.Collections.Settings;
+using BorrehSoft.ApolloGeese.CoreTypes;
+using System.Text.RegularExpressions;
+
+namespace InputProcessing
+{
+	public class DateField : Field<string>
+	{
+		protected override bool CheckValid (object rawInput)
+		{
+			DateTime dummy;
+			return DateTime.TryParse (rawInput.ToString (), out dummy);
+		}
+	}
+}
+
