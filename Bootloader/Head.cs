1c5517041c0dd6035a0add6244ed7e25884593f0
Rob Tierolff <teh@borreh.nl>
2016-05-26 14:07:59 +0200

    Logdirectory and help

--- Bootloader/Head.cs
+++ Bootloader/Head.cs
@@ -44,44 +44,69 @@ namespace BorrehSoft.ApolloGeese
 			bool pluginsFromBin = false;
 			SimpleInteraction miscArgs = new SimpleInteraction ();
 
-			StartLog(logfolder);
-
-			while (CommandLineArguments.Count > 0) {
-				string paramAhead = CommandLineArguments.Dequeue();
-				if (paramAhead.ToLower () == "-b") {
-					runbranch = CommandLineArguments.Dequeue ();
-					Secretary.Report (5, "Branch name: ", runbranch);
-				} else if (paramAhead.ToLower () == "-c") {
-					config = CommandLineArguments.Dequeue ();
-					Secretary.Report (5, "Config file: ", config);
-				} else if (paramAhead.ToLower () == "-l") {
-					logfolder = CommandLineArguments.Dequeue ();
-					Secretary.Report (5, "Logfile: ", config);				
-				} else if (paramAhead.ToLower () == "-pfc") {
-					pluginsFromConfig = bool.Parse (CommandLineArguments.Dequeue ());
-					Secretary.Report (5, "Plugins from Config: ", pluginsFromConfig.ToString());
-				} else if (paramAhead.ToLower () == "-pfb") {
-					pluginsFromBin = bool.Parse (CommandLineArguments.Dequeue ());
-					Secretary.Report (5, "Plugins from Bin: ", pluginsFromBin.ToString());
-				} else if (runbranch.Length > 0) {
-					string[] pair = paramAhead.ToLower ().Split ('=');
-
-					if (pair.Length == 2) {
-						miscArgs [pair [0]] = pair [1];
-						Secretary.Report (5, pair [0], "=", pair [1]);
-					} else {
-						throw new Exception (
-							"Interaction variables need to be supplied as key=value-pair");
-					}
-				} else {
-					throw new Exception ("Not sure what to do with " + paramAhead.ToLower ());
+			try {
+				int logParamIndex = Array.IndexOf (args, "-l");
+				if (logParamIndex > -1) {
+					logfolder = args [logParamIndex + 1];
 				}
-			}
-		
-			Map<Service> services = InstanceLoader.GetInstances (config, pluginsFromConfig, pluginsFromBin);
+				StartLog(logfolder);
 
-			if (runbranch.Length > 0) {
-				services [runbranch].TryProcess (miscArgs);
+				while (CommandLineArguments.Count > 0) {
+					string paramAhead = CommandLineArguments.Dequeue();
+					if (paramAhead.ToLower().Contains("-h")) {
+						throw new Exception("Help requested");
+					} else if (paramAhead.ToLower () == "-b") {
+						runbranch = CommandLineArguments.Dequeue ();
+						Secretary.Report (5, "Branch name: ", runbranch);
+					} else if (paramAhead.ToLower () == "-l") {
+						CommandLineArguments.Dequeue ();
+						Secretary.Report (5, "Logfolder: ", logfolder);   
+					} else if (paramAhead.ToLower () == "-c") {
+						config = CommandLineArguments.Dequeue ();
+						Secretary.Report (5, "Config file: ", config);
+					} else if (paramAhead.ToLower () == "-pfc") {
+						pluginsFromConfig = bool.Parse (CommandLineArguments.Dequeue ());
+						Secretary.Report (5, "Plugins from Config: ", pluginsFromConfig.ToString());
+					} else if (paramAhead.ToLower () == "-pfb") {
+						pluginsFromBin = bool.Parse (CommandLineArguments.Dequeue ());
+						Secretary.Report (5, "Plugins from Bin: ", pluginsFromBin.ToString());
+					} else if (runbranch.Length > 0) {
+						string[] pair = paramAhead.ToLower ().Split ('=');
+
+						if (pair.Length == 2) {
+							miscArgs [pair [0]] = pair [1];
+							Secretary.Report (5, pair [0], "=", pair [1]);
+						} else {
+							throw new Exception (
+								"Interaction variables need to be supplied as key=value-pair");
+						}
+					} else {
+						throw new Exception ("Not sure what to do with " + paramAhead.ToLower ());
+					}
+				}
+
+				Map<Service> services = InstanceLoader.GetInstances (config, pluginsFromConfig, pluginsFromBin);
+
+				if (runbranch.Length > 0) {
+					services [runbranch].TryProcess (miscArgs);
+				}
+			} catch(Exception ex) {
+				Console.WriteLine ("ApolloGeese\n" +
+					"2013-2016 Rob Tierolff");
+				Console.WriteLine ("Message: {0}", ex.Message);
+				Console.WriteLine (
+					"-h: This information\n" +
+					"-c [file]: Specify bootstrapper config\n" +
+					"-b [identifier]: Specify startup branch\n" +
+					"-l [directory]: Logging directory\n" +
+					"-pfc [*true|false]: Load plugin assemblies from plugins-array in bootstrapper config.\n" +
+					"-pfb [*false|true]: Load plugin assemblies fron installation directory.\n" +
+					"key=value: Extra context to feed into the startup branch");
+
+				Secretary.LatestLog.Dispose ();
+				Secretary.Report (5, "Exiting now");
+
+				Environment.Exit (0);
 			}
 		}
 	}
