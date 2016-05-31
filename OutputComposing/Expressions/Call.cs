e9a546e091bf7ce1839e9e0804983c8b2a7f9f1f
Rob Tierolff <teh@borreh.nl>
2016-06-01 00:10:55 +0200

    Misc template branch

--- OutputComposing/Expressions/Call.cs
+++ OutputComposing/Expressions/Call.cs
@@ -15,19 +15,33 @@ namespace BorrehSoft.ApolloGeese.Extensions.OutputComposing
 	class Call : NamedExpression
 	{
 		Template Caller;
+		Map<Service> Branches;
+		const string TEMPLATE_VARIABLE = "templatevariable";
 
 		public Call (string rawExpression, Template template)
 		{
 			this.Expression = rawExpression;
 			this.Caller = template;
+			this.Branches = this.Caller.Branches;
 		}
 		
 		public override bool TryWriteTo (StreamWriter writer, IInteraction context) {
 			// they should've never told me about branch prediction
-			return this.Caller.Branches.Has (
-				this.Name) && this.Caller.Branches.Get (
-					this.Name).TryProcess (
-						context);
+
+			Service called = null;
+			IInteraction callContext;
+
+			if (Branches.Has (this.Name)) {
+				called = Branches [this.Name];
+				callContext = context;
+			} else if (Branches.Has (TEMPLATE_VARIABLE)) {
+				called = Branches [TEMPLATE_VARIABLE];
+				callContext = new SimpleInteraction (context, TEMPLATE_VARIABLE, this.Name);
+			} else {
+				return false;
+			}
+
+			return called.TryProcess (callContext);
 		}
 	}
 }
