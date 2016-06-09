b66aa18ca54d02ffefef5b48e23502512fefc401
Rob Tierolff <teh@borreh.nl>
2016-06-09 21:12:43 +0200

    Relative paths, fail/succ

--- Filesystem/WriteToFile.cs
+++ Filesystem/WriteToFile.cs
@@ -11,10 +11,13 @@ namespace Filesystem
 
 		public string ValidRootPath { get; set; }
 
+		public bool IsRelative { get; set; }
+
 		protected override void Initialize (Settings settings)
 		{
 			this.PathVariable = settings.GetString ("pathvariable", "fullpath");
 			this.ValidRootPath = settings.GetString ("rootpath");
+			this.IsRelative = settings.GetBool ("isrelative", false);
 		}
 
 		protected override bool Process (IInteraction parameters)
@@ -22,11 +25,31 @@ namespace Filesystem
 			IInteraction dataSource;
 
 			var dataBody = Closest<IIncomingBodiedInteraction>.From (parameters);
-			var fileName = Fallback<string>.From (parameters, this.PathVariable);
+			string providedFilename = Fallback<string>.From (parameters, this.PathVariable);
+			string fileName;
+
+			if (IsRelative) {
+				fileName = string.Format (
+					"{0}{1}{2}",
+					this.ValidRootPath.TrimEnd ('\\', '/'),
+					Path.DirectorySeparatorChar,
+					providedFilename
+				);
+			} else {
+				fileName = providedFilename;
+			}
+
+			FileInfo file = new FileInfo (fileName);
+
+			if (!file.FullName.StartsWith (this.ValidRootPath)) {
+				return Failure.TryProcess (parameters);
+			}
 
 			using (FileStream fileStream = File.OpenWrite (fileName)) {
 				dataBody.IncomingBody.CopyTo (fileStream);
 			}
+
+			return Successful.TryProcess (parameters);
 		}
 	}
 }
