d22ae1df35ae7ae0c3ac4fd02b0c3b5857806b16
Rob Tierolff <teh@borreh.nl>
2016-06-09 21:04:21 +0200

    WriteToFile service

---
+++ Filesystem/WriteToFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using BorrehSoft.ApolloGeese.CoreTypes;
+using BorrehSoft.Utensils.Collections.Settings;
+using System.IO;
+
+namespace Filesystem
+{
+	public class WriteToFile : TwoBranchedService
+	{
+		public string PathVariable { get; set; }
+
+		public string ValidRootPath { get; set; }
+
+		protected override void Initialize (Settings settings)
+		{
+			this.PathVariable = settings.GetString ("pathvariable", "fullpath");
+			this.ValidRootPath = settings.GetString ("rootpath");
+		}
+
+		protected override bool Process (IInteraction parameters)
+		{
+			IInteraction dataSource;
+
+			var dataBody = Closest<IIncomingBodiedInteraction>.From (parameters);
+			var fileName = Fallback<string>.From (parameters, this.PathVariable);
+
+			using (FileStream fileStream = File.OpenWrite (fileName)) {
+				dataBody.IncomingBody.CopyTo (fileStream);
+			}
+		}
+	}
+}
+
