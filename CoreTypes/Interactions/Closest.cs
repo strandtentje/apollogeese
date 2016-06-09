6d3b00cb0d1a694f9eeb5654ab61647d41f523c7
Rob Tierolff <teh@borreh.nl>
2016-06-09 21:04:10 +0200

    Useful utility classes for retrieving underlying interactions and variables

---
+++ CoreTypes/Interactions/Closest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BorrehSoft.ApolloGeese.CoreTypes
+{
+	public static class Closest<T> where T : IInteraction
+	{
+		public class InteractionNotFoundException : Exception 
+		{
+			public InteractionNotFoundException(Type ofType) : base(
+				string.Format(
+					"Interaction of type {0} not found", 
+					ofType.Name
+				)
+			) { }
+		}
+
+		public static T From(IInteraction parameters)
+		{
+			IInteraction candidate;
+			if (parameters.TryGetClosest (typeof(T), out candidate)) {
+				return (T)candidate;
+			} else {
+				throw new InteractionNotFoundException (typeof(T));
+			}
+		}
+	}
+}
+
---
+++ CoreTypes/Interactions/Fallback.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BorrehSoft.ApolloGeese.CoreTypes
+{
+	public static class Fallback<T>
+	{
+		public class VariableNotFoundException : Exception
+		{
+			public VariableNotFoundException(string name, Type ofType) : base(
+				string.Format(
+					"Variable with name {0} of type {1} not found",
+					name, ofType.Name
+				)
+			) { }
+		}
+
+		public static T From(IInteraction parameters, string name)
+		{
+			object candidate;
+			if (parameters.TryGetFallback (name, out candidate) && (candidate is T)) {
+				return (T)candidate;
+			} else {
+				throw new VariableNotFoundException (name, typeof(T));
+			}
+		}
+	}
+}
+
