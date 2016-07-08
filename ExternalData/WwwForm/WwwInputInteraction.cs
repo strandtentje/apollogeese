edda7d1e208c2557570969f3c60115bb8a053cce
Rob Tierolff <teh@borreh.nl>
2016-07-08 18:55:03 +0200

    Unneeded factorymethod

--- ExternalData/WwwForm/WwwInputInteraction.cs
+++ ExternalData/WwwForm/WwwInputInteraction.cs
@@ -8,18 +8,6 @@ namespace ExternalData
 {
 	class WwwInputInteraction : NameValueInteraction
 	{
-		public static WwwInputInteraction FromPair (string pair, IInteraction parameters)
-		{
-			string[] splitPair = pair.Split ('=');
-
-			string name = splitPair [0];
-			string value = "";
-			if (splitPair.Length > 1)
-				value = HttpUtility.UrlDecode (splitPair [1]);	
-
-			return new WwwInputInteraction (name, value, parameters);
-		}
-			
 		public WwwInputInteraction (string name, string value, IInteraction parent) : base (parent) 
 		{
 			this.Name = name;
