729ebee23d65d52cb7fb4434f3e33c5d3566549c
Rob Tierolff <teh@borreh.nl>
2016-07-08 18:53:04 +0200

    De-overengineered datainteraction

--- BetterData/DataInteraction.cs
+++ BetterData/DataInteraction.cs
@@ -7,38 +7,16 @@ namespace BetterData
 {
 	class DataInteraction : SimpleInteraction
 	{
-		string[] ColumnNames;
-
-		object[] CellValues;
-
 		public DataInteraction (
 			IInteraction parameters, 
 			string[] columnNames, 
 			object[] values) : base(
 				parameters)
 		{
-			this.ColumnNames = columnNames;
-			this.CellValues = values;
-		}
-
-		public override bool Has (string key)
-		{
-			bool hasColumn = Array.IndexOf (ColumnNames, key) > -1;
-
-			return hasColumn || base.Has (key);
-		}
-
-		public override object Get (string key)
-		{
-			int columnIndex = Array.IndexOf (ColumnNames, key);
-
-			if (columnIndex < 0) {
-				return base.Get (key);
-			} else {
-				return CellValues [columnIndex];
+			for (int columnIndex = 0; columnIndex < columnNames.Length; columnIndex++) {
+				this [columnNames [columnIndex]] = values [columnIndex];
 			}
 		}
-
 	}
 
 }
