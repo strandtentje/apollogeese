730a00f27392db74a448fdaf644747555791ad60
Rob Tierolff <teh@borreh.nl>
2016-11-01 14:18:37 +0100

    Got rid of outgoing writer interaction because it solved half a problem and introduced string processor interaction

---
+++ CoreTypes/Interactions/Simple/StringProcessorInteraction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace BorrehSoft.ApolloGeese.CoreTypes
+{
+	public class StringProcessorInteraction : SimpleOutgoingInteraction
+	{
+		MemoryStream UnderlyingStream;
+		IOutgoingBodiedInteraction SinkInteraction;
+
+		private StringProcessorInteraction (
+			MemoryStream underlyingStream,
+			IInteraction parent,
+			IOutgoingBodiedInteraction sinkInteraction
+		) : base(
+			underlyingStream, 
+			sinkInteraction.Encoding,
+			parent
+		) {
+			this.UnderlyingStream = underlyingStream;
+			this.SinkInteraction = sinkInteraction;
+		}
+
+		public static StringProcessorInteraction From(IInteraction parent) {
+			return new StringProcessorInteraction (
+				new MemoryStream(),
+				parent, 
+				Closest<IOutgoingBodiedInteraction>.From (parent)
+			);
+		}
+
+		public void Run(Func<string, string> processor) 
+		{
+			Done ();
+			UnderlyingStream.Position = 0;
+
+			byte[] data;
+
+			using (StreamReader reader = new StreamReader (UnderlyingStream))
+				data = Encoding.GetBytes (processor (reader.ReadToEnd ()));
+				
+			SinkInteraction.OutgoingBody.Write (data, 0, data.Length);
+		}
+	}
+}
+
