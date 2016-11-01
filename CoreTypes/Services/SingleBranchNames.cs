89f2140010dd637c4ca1e1ab0f5879ca8c08b253
Rob Tierolff <teh@borreh.nl>
2016-11-01 16:03:24 +0100

    Introduce SingleBranchService to replace jungle of names of single branches

---
+++ CoreTypes/Services/SingleBranchNames.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BorrehSoft.ApolloGeese.CoreTypes
+{
+	public class SingleBranchNames
+	{
+		public const string Begin = "begin";
+		public const string Content = "content";
+		public const string Continue = "continue";
+		public const string Data = "data";
+		public const string Http = "http";
+		public const string Init = "init";
+		public const string Next = "next";
+		public const string Offload = "offload";
+		public const string Proceed = "proceed";
+		public const string Request = "request";
+		public const string Source = "source";
+		public const string Subject = "subject";
+		public const string With = "_with";
+	}
+}
+
---
+++ CoreTypes/Services/SingleBranchService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using BorrehSoft.Utensils.Collections.Maps;
+
+namespace BorrehSoft.ApolloGeese.CoreTypes
+{
+	public abstract class SingleBranchService : Service
+	{
+		protected Service WithBranch { get; private set; }
+
+		protected override sealed void HandleBranchChanged (object sender, ItemChangedEventArgs<Service> e)
+		{
+			switch (e.Name) {
+			case SingleBranchNames.With:
+			case SingleBranchNames.Begin:
+			case SingleBranchNames.Content:
+			case SingleBranchNames.Continue:
+			case SingleBranchNames.Data:
+			case SingleBranchNames.Http:
+			case SingleBranchNames.Init:
+			case SingleBranchNames.Next:
+			case SingleBranchNames.Offload:
+			case SingleBranchNames.Proceed:
+			case SingleBranchNames.Request:
+			case SingleBranchNames.Source:
+			case SingleBranchNames.Subject:
+				this.WithBranch = e.NewValue;
+			default:
+				break;
+			}
+		}
+	}
+}
+
