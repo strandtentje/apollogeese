01f161718c61ee2ad42abb60a4761ae4ab5e6321
Rob Tierolff <teh@borreh.nl>
2017-05-19 01:47:59 +0200

    Branching utility class

---
+++ CoreTypes/Branch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BorrehSoft.ApolloGeese.CoreTypes
+{
+	public class Branch
+	{
+		public IInteraction Interaction { get; private set; }
+
+		public bool Success { get; private set; }
+
+		public static Branch For(IInteraction interaction) 
+		{
+			return new Branch() { Interaction = interaction, Success = true };
+		}
+
+		public Branch Optional(params Service[] services)
+		{
+			foreach (var service in services) {
+				Success &= (service == null) || service.TryProcess(Interaction);
+			}
+			return this;
+		}
+
+		public bool Finish() 
+		{
+			return Success;
+		}
+	}
+}
+
