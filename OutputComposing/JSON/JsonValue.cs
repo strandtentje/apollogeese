7147550a8afc3201ff96922e099a66739effab18
Rob Tierolff <teh@borreh.nl>
2017-05-24 12:18:43 +0200

    Json Arrays

--- OutputComposing/JSON/JsonValue.cs
+++ OutputComposing/JSON/JsonValue.cs
@@ -50,31 +50,48 @@ namespace BorrehSoft.ApolloGeese.Extensions.OutputComposing
 			}
 		}
 
-		protected override bool Process (IInteraction parameters)
+		private JsonObject CreateJsonObjectFromInteraction (IInteraction parameters)
 		{
-			var outgoing = Closest<IOutgoingBodiedInteraction>.From (parameters);
-
-			if (outgoing is IHttpInteraction) {
-				try {
-					((IHttpInteraction)outgoing).SetContentType ("application/json");
-				} catch (HttpException ex) {
-					Secretary.Report (5, "Failed setting json mimetype.");
-				}
-			}
-
 			object value;
-
 			JsonObject values = new JsonObject ();
 			foreach (var assignment in ValueAssignments) {
 				values [assignment.Item1] = new JsonPrimitive (assignment.Item2);
 			}
 			foreach (var remapping in ValueNameRemappings) {
-				if (parameters.TryGetFallback (
-					    remapping.Item2, out value)) {
+				if (parameters.TryGetFallback (remapping.Item2, out value)) {
 					values [remapping.Item1] = JsonPrimitive.From (value);
 				}
 			}
-			Writer.Into (outgoing).Append (values);
+			foreach (var branch in Branches.Dictionary) {
+				JsonArrayInteraction interaction = new JsonArrayInteraction (parameters);
+				if (branch.Value.TryProcess (interaction)) {
+					values [branch.Key] = interaction.Array;
+				}
+			}
+			return values;
+		}
+
+		private void PutJsonObjectIntoInteraction (IInteraction parameters, JsonObject values)
+		{			
+			IInteraction candidate;
+			if (parameters.TryGetClosest(typeof(JsonArrayInteraction), out candidate)) {
+				((JsonArrayInteraction)candidate).Add(values);
+			} else if (parameters.TryGetClosest(typeof(IOutgoingBodiedInteraction), out candidate)) {
+				if (candidate is IHttpInteraction) {
+					try {
+						((IHttpInteraction)candidate).SetContentType ("application/json");
+					}
+					catch (HttpException ex) {
+						Secretary.Report (5, "Failed setting json mimetype.");
+					}
+				}
+				Writer.Into ((IOutgoingBodiedInteraction)candidate).Append (values);
+			}
+		}
+
+		protected override bool Process (IInteraction parameters)
+		{
+			PutJsonObjectIntoInteraction (parameters, CreateJsonObjectFromInteraction (parameters));
 
 			return true;
 		}
