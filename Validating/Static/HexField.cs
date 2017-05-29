499859023f5d1d1daabdba0448eb445aa8d988de
Rob Tierolff <teh@borreh.nl>
2017-05-30 01:20:41 +0200

    Hex field

---
+++ Validating/Static/HexField.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using InputProcessing;
+using BorrehSoft.Utilities.Collections.Settings;
+
+namespace Static
+{
+	public class HexField : TextField
+	{
+		protected override void Initialize (Settings settings)
+		{
+			settings["pattern"] = "^([a-f|0-9][a-f|0-9])+$";
+			base.Initialize (settings);
+		}
+	}
+}
+
