07a5e0c5f3914fe90ff5b047873c868594403063
Rob Tierolff <teh@borreh.nl>
2017-05-30 01:20:33 +0200

    New Guid service

---
+++ CoreTypes/Services/Simple/NewGuid.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BorrehSoft.ApolloGeese.CoreTypes;
+using BorrehSoft.Utilities.Collections.Settings;
+
+namespace Services.Simple
+{
+	public class NewGuid : SingleBranchService
+	{
+		public override string Description {
+			get {
+				return "Guid Maker";
+			}
+		}
+
+		public override void LoadDefaultParameters (object defaultParameter)
+		{
+			Settings["variable"] = defaultParameter;
+		}
+
+		string Variable;
+
+		string Format;
+
+		protected override void Initialize (Settings settings)
+		{
+			this.Variable = settings.GetString("variable", "guid");
+			this.Format = settings.GetString("format", "N");
+		}
+
+		protected override bool Process (IInteraction parameters)
+		{
+			return WithBranch.TryProcess(new SimpleInteraction(parameters,Variable, Guid.NewGuid().ToString(this.Format)));
+		}
+	}
+}
+
